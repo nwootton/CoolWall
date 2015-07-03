c4d0f3d861f91a2e2390e2685b469378bfc5d503
Nick Wootton <Nicholas.Wootton@bskyb.com>
2015-07-03 13:43:31 +0100

    Wrong DB name in config. Referred to another project instead of the db that gets created in the BuildDB.sql script

--- src/www/config/config.php
+++ src/www/config/config.php
@@ -1,18 +1,18 @@
 <?php
 
-// Database 
+// Database
 $config['db'] = array(
     'DB_SERVER' 	=> 'localhost',
 	'DB_USERNAME' 	=> 'root',
 	'DB_PASSWORD' 	=> 'root',
-	'DB_DATABASE' 	=> 'SocialEPG'
+	'DB_DATABASE' 	=> 'HeatMap'
 );
 
 
 //BoardSize - NOT used as far as I can tell!
 $config['board'] = array(
 	'X'			=> '1920',
-	'Y'			=> '1080'		
+	'Y'			=> '1080'
 );
 
 return $config;
